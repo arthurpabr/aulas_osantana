b675b89140008260534f51feaf938a3198cb21ea
Arthur R Marcondes <arthur.marcondes@ifsuldeminas.edu.br>
2016-01-19 14:35:58 -0200

    Todos os exercícios até vídeo 10 - começar no vídeo 11

--- agenda/views.py
+++ agenda/views.py
@@ -1,6 +1,27 @@
 # _*_ encoding: utf-8 _*_
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from agenda.models import ItemAgenda
+from agenda.forms import ItemAgendaForm
 
-def index(request):
-	return HttpResponse(u"Olá mundo")
+def lista(request):
+	lista_de_itens = ItemAgenda.objects.all()
+	return render(request, 'lista.html', {"lista_de_itens": lista_de_itens})
+
+def adiciona(request):
+	form = ItemAgendaForm()
+	if request.method == 'POST':
+		form = ItemAgendaForm(request.POST)
+		if form.is_valid():
+			dados = form.cleaned_data
+			item = ItemAgenda(
+				data = dados['data'],
+				hora = dados['hora'],
+				titulo = dados['titulo'],
+				descricao = dados['descricao']
+			)
+			item.save()
+			return render(request,'salvo.html',{})		
+	return render(request, 'adiciona.html', {"form": form})
+
+	def detalha(request):
+		pass
--- aulas_osantana/urls.py
+++ aulas_osantana/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from agenda import views
 
 urlpatterns = patterns('',
-    url(r'^$', "agenda.views.index"),
+    url(r'^$', views.lista, name="lista"),
+    url(r'^adiciona/$', views.adiciona, name="adiciona"),
 )
